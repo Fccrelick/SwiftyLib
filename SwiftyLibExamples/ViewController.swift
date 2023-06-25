5aacd0fc0f30322c610a206b167499a1e1c43ceb
Fernando Crelick <fernando.crelick@compasso.com.br>
2023-06-25 15:40:12 -0300

    Initial commit

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Fernando Crelick on 24/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
